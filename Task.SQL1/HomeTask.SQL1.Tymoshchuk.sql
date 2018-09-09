9c4e4f4f2102e7847c3351700efd7621203bb885
uadeveloper17 <uadeveloper17@gmail.com>
2018-09-09 12:07:40 +0300

    sft_tasks1
    Tasks

---
+++ Task.SQL1/HomeTask.SQL1.Tymoshchuk.sql
@@ -0,0 +1,79 @@
+﻿--1) Update the Employees table, so it contains the HireDate values from 2014 till the current (2019) year.
+
+Select *
+FROM dbo.Employees
+
+UPDATE dbo.Employees SET HireDate = '2014-05-01 00:00:000'
+WHERE EmployeeID = '1';
+
+UPDATE dbo.Employees SET HireDate = '1992-06-10 00:00:000'
+WHERE EmployeeID = '2';
+
+UPDATE dbo.Employees SET HireDate = '2015-04-01 00:00:000'
+WHERE EmployeeID = '3';
+
+UPDATE dbo.Employees SET HireDate = '2017-05-03 00:00:000'
+WHERE EmployeeID = '4';
+
+UPDATE dbo.Employees SET HireDate = '2019-10-17 00:00:000'
+WHERE EmployeeID = '5';
+
+UPDATE dbo.Employees SET HireDate = '2018-10-17 00:00:000'
+WHERE EmployeeID = '6';
+
+UPDATE dbo.Employees SET HireDate = '2016-01-02 00:00:000'
+WHERE EmployeeID = '7';
+
+--2)Delete records from the Products table where ReorderLevel values is equal to 30. 
+
+SELECT *
+FROM dbo.Products
+
+EXEC sp_msforeachtable 'ALTER TABLE ? NOCHECK CONSTRAINT all'
+
+DELETE FROM dbo.Products
+WHERE ReorderLevel = 30;
+
+--3)Get the list of titles and corresponding employees, who are working in each department (the list of columns in the result set from the Employee table is optional).
+
+SELECT Title, FirstName, LastName
+FROM dbo.Employees
+ORDER BY Title DESC
+
+--4)Get the list of suppliers, which are located in USA and have a specified region.
+
+SELECT SupplierID, ContactName, Country, Region
+FROM dbo.Suppliers
+WHERE Region IS NOT NULL AND Country = 'USA';
+
+--5)Get the amount of products that were delivered by each supplier (company), which have a discount from the Unit Price more than 10%. (order by companyid)
+
+SELECT COUNT(dbo.Products.ProductName) AS Quantity, dbo.Suppliers.CompanyName
+FROM dbo.[Order Details] 
+JOIN dbo.Products ON dbo.[Order Details].ProductID = dbo.Products.ProductID
+JOIN dbo.Suppliers ON dbo.Suppliers.SupplierID = dbo.Products.SupplierID
+WHERE dbo.[Order Details].Discount > 0.1
+GROUP BY dbo.Suppliers.CompanyName
+
+--6)Get the top five product categories with the list of the most buyable products in European countries.
+
+SELECT top 5 Suppliers.Country, Products.CategoryID, Products.UnitsOnOrder
+FROM Products
+INNER JOIN Suppliers ON Products.SupplierID = Suppliers.SupplierID
+WHERE Suppliers.Country IN ('Sweden', 'Finland', 'UK', 'Italy', 'Netherlands', 'Norway', 'France', 'Germany', 'Denmark', 'Spain')
+ORDER BY Products.UnitsOnOrder DESC
+
+
+--7)Get the First Name, Last Name and Title of Managers and their subordinates.
+
+SELECT FirstName, LastName, Title
+FROM Employees
+WHERE Title LIKE '%Manager%'
+OR ReportsTo = (SELECT EmployeeID FROM Employees WHERE Title LIKE '%Manager%')
+
+--The next queries are optional:
+--	Get the list of data about employees: First Name, Last Name, Title, HireDate who was hired this year.
+
+SELECT FirstName, LastName, Title, HireDate
+FROM dbo.Employees
+WHERE HireDate BETWEEN '2018.01.01 00:00:00.000' AND '2018.12.31 23:59:59.999'
---
+++ Task.SQL2/Task.SQL2.Tymoshchuk.sql
@@ -0,0 +1,126 @@
+-- 1) Clone data from the Shippers table to the NewShippers table.
+
+--SELECT *
+--INTO NewShippers
+--FROM Shippers
+
+--Select * FROM NewShippers
+
+--Select * FROM Shippers
+
+-- 2) Find the set of products (Product Name) and maximum value of units in stock for each one,
+-- which is in the range from 25 to 50. Represent records from the min to max value of units in stock.
+
+--Select ProductName, Products.UnitsInStock
+--From Products
+--Where UnitsInStock > 25 AND UnitsInStock < 50
+--Order by UnitsInStock ASC
+
+--  3) Get the list of total quantities of ordered products which consists of:
+--  total quantity ordered in Germany and the total quantiy* 0.7% of products ordered in Sweden.
+--  (Result should contain 2 rows)+
+
+Select SUM([Order Details].Quantity)*0.007 AS TotalQuantity,  Suppliers.Country
+From Products
+inner join [Order Details]
+ON Products.CategoryID = [Order Details].ProductID
+inner join Suppliers
+ON Products.SupplierID = Suppliers.SupplierID
+Where Country = 'Sweden'
+GROUP BY Country
+UNION
+Select SUM([Order Details].Quantity) AS TotalQuantity,  Suppliers.Country
+From Products
+inner join [Order Details]
+ON Products.CategoryID = [Order Details].ProductID
+inner join Suppliers
+ON Products.SupplierID = Suppliers.SupplierID
+Where Country LIKE '%Germany%'
+GROUP BY Country
+
+
+--  4) Find the list of different countries in Employees and Customers tables.
+
+--SELECT Country
+--From Customers
+--EXCEPT
+--SELECT Country
+--From Employees
+
+--  5) Find the list of the same Postal Codes between Suppliers and Employees tables.
+
+--Select PostalCode From Suppliers
+--INTERSECT
+--Select PostalCode From Employees
+
+--  6) Find the top region from which sales specialists were hired.
+
+--SELECT MAX(Region) AS TopRegion
+--From Employees
+--WHERE Title LiKE '%Sales%'
+
+--  7) Get two lists of products: with a price < 50.00 with a discountinued flag and < 50  without a discountinued flag.
+
+--select * from Products
+--Where UnitPrice < 50 AND Discontinued = 0;
+
+--select * from Products
+--Where UnitPrice < 50 AND Discontinued = 1;
+
+--  8) Create new table NewProducts based on the Products table with only discountinued products. 
+--  Compare data sets between Products and NewProducts tables. (Check that only discountinued products are inserted).
+
+--CREATE TABLE NewProducts (
+--ProductID INT NOT NULL,
+--ProductName nvarchar(40) NOT NULL,
+--SupplierID INT NULL,
+--CategoryID INT NULL,
+--QuantityPerUnit nvarchar(40) NULL,
+--UnitPrice MONEY NULL,
+--UnitsInStock SMALLINT NULL,
+--UnitsOnOrder SMALLINT NULL,
+--ReorderLevel SMALLINT NULL,
+--Discontinued BIT NOT NULL,
+--PRIMARY KEY (ProductID),
+--FOREIGN KEY (SupplierID) REFERENCES Suppliers (SupplierID),
+--FOREIGN KEY (CategoryID) REFERENCES Categories (CategoryID)
+--); 
+
+--INSERT INTO NewProducts
+--SELECT *
+--FROM Products
+--WHERE Discontinued = 1
+
+--Select * FROM NewProducts
+
+--SELECT Discontinued From Products
+--INTERSECT
+--SELECT Discontinued From NewProducts
+
+--*******************************************************************************************
+
+--SELECT *
+--INTO NewProducts
+--FROM Products
+--WHERE Discontinued = 1;
+
+
+--Select Discontinued From Products
+--INTERSECT
+--Select Discontinued From NewProducts
+
+
+--The 9th query is optional 
+-- * Get the list of orders, where a required date is bigger than the Shipped date 
+--   (compare in days) and Ship Region is not specified.
+
+--select * from Orders
+
+--Select *
+--FROM Orders
+--Where Datediff(day, ShippedDate,RequiredDate) != 0 AND ShipRegion IS NULL;
+
+
+--SELECT OrderID, CustomerID, ShipName, ShippedDate, RequiredDate, Datediff (day, RequiredDate,ShippedDate) as Differences
+--FROM Orders
+--WHERE RequiredDate > 0 AND ShipRegion IS NULL
